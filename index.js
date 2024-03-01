a4ca5970789b71ad64a5010df16d0542a96954b3
SimoneRimedio <rimediosimone1@gmail.com>
2024-03-01 13:43:19 +0100

    index edited

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-require('dotenv').config();
 const app = express();
-const port = 3000;
+
+require('dotenv').config();
+
+const authAdminRouter = require("./src/routes/admin/authAdminRoutes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +13,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+app.use("/admin",authAdminRouter);
 
-app.listen(port, () => {
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
