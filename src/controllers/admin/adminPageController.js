0e8d2320bca519946ff5ddbb749badd040ae86ce
SimoneRimedio <rimediosimone1@gmail.com>
2024-03-27 13:35:27 +0100

    admin page added

---
+++ src/controllers/admin/adminPageController.js
@@ -0,0 +1,10 @@
+const Prisma = require("../../connection/connection");
+
+const dbUpdate = (res,req) =>{
+
+    const file = req.body.file;
+
+    //inserire query prisma per aggiornare tuttoil db
+
+}
+
