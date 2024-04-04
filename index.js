ccd84e8da983393252e0b4b904d4b7d61e9322e1
SimoneRimedio <rimediosimone1@gmail.com>
2024-04-04 11:30:31 +0200

    .

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const cors = require("cors")
 const bodyParser = require("body-parser");
 const app = express();
 
-require('dotenv').config();
-
 const authAdminRouter = require("./src/routes/admin/authAdminRoutes");
+
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
 app.use("/admin",authAdminRouter);
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
