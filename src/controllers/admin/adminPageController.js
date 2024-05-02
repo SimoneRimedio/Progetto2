768170085b826296572dff6cadd01e561bbc8823
SimoneRimedio <rimediosimone1@gmail.com>
2024-05-03 00:05:22 +0200

    dbupdate function fixed (pending)

--- src/controllers/admin/adminPageController.js
+++ src/controllers/admin/adminPageController.js
@@ -39,23 +39,58 @@ const dbUpdate = (req, res) => {
             res.status(500).send('Si è verificato un errore durante l\'analisi del file CSV.');
         });
 
-    async function updateDatabase(parsedData) {
-        try {
-               for (const data of parsedData) {
-                await prisma.schedule.upsert({
-                    where: { NUMERO: data.NUMERO },
-                    update: data,
-                    create: data,
-                });
+        async function updateDatabase(parsedData) {
+            try {
+                for (const data of parsedData) {
+                    await prisma.schedule.upsert({
+                        where: { NUMERO: data.NUMERO },
+                        update: {
+                            NUMERO: data.NUMERO,
+                            DURATA: data.DURATA,
+                            FREQUENZA: data.FREQUENZA,
+                            MAT_COD: data.MAT_COD,
+                            MAT_NOME: data.MAT_NOME,
+                            DOC_COGN: data.DOC_COGN,
+                            DOC_NOME: data.DOC_NOME,
+                            CLASSE: data.CLASSE,
+                            AULA: data.AULA,
+                            PERIODICITA: data.PERIODICITA,
+                            SPECIFICA: data.SPECIFICA,
+                            CO_DOC_: data['CO-DOC.'],
+                            COEFF_: data['COEFF.'],
+                            GIORNO: data.GIORNO,
+                            O_INIZIO: data['O.INIZIO'],
+                            ALUNNI: data.ALUNNI
+                        },
+                        create: {
+                            NUMERO: data.NUMERO,
+                            DURATA: data.DURATA,
+                            FREQUENZA: data.FREQUENZA,
+                            MAT_COD: data.MAT_COD,
+                            MAT_NOME: data.MAT_NOME,
+                            DOC_COGN: data.DOC_COGN,
+                            DOC_NOME: data.DOC_NOME,
+                            CLASSE: data.CLASSE,
+                            AULA: data.AULA,
+                            PERIODICITA: data.PERIODICITA,
+                            SPECIFICA: data.SPECIFICA,
+                            CO_DOC_: data['CO-DOC.'],
+                            COEFF_: data['COEFF.'],
+                            GIORNO: data.GIORNO,
+                            O_INIZIO: data['O.INIZIO'],
+                            ALUNNI: data.ALUNNI
+                        }
+                    });
+                }
+        
+                console.log('Database aggiornato con successo!');
+            } catch (error) {
+                console.error('Si è verificato un errore durante l\'aggiornamento del database:', error);
+            } finally {
+                await prisma.$disconnect();
             }
-
-            console.log('Database aggiornato con successo!');
-        } catch (error) {
-            console.error('Si è verificato un errore durante l\'aggiornamento del database:', error);
-        } finally {
-            await prisma.$disconnect();
         }
-    }
+        
 }
 
 module.exports = dbUpdate;
