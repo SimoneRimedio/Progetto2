809c2385c9e856fc78ab1f6dc78e65b6fc2ae056
davide <davide.martinelli@jcmaxwell.it>
2024-05-29 09:03:29 +0200

    sistemazione tabella (non responsive)

--- front/src/components/Table/Schedule.jsx
+++ front/src/components/Table/Schedule.jsx
@@ -36,55 +36,56 @@ const TabellaOrario = ({ data, info }) => {
   let optionValue = urlParams.get('option');
   console.log('Valore del parametro "option":', optionValue);
 
-  if (optionValue == "Docenti") {
+  if (optionValue === "Docenti") {
     ris = "Il docente " + searchValue + " si trova in " + info.AULA;
-  } else if (optionValue == "Classi") {
+  } else if (optionValue === "Classi") {
     ris = "La classe " + searchValue + " si trova in " + info.AULA;
-  } else if (optionValue == "Aule") {
+  } else if (optionValue === "Aule") {
     ris = "Nell'aula " + searchValue + " si trova la classe " + info.CLASSE;
   }
 
   const [coloriMaterie] = useState({});
 
-
-
   return (
-    <div>
-    <p class="bg-white text-gray-800 text-lg font-medium p-4 rounded-md shadow-sm text-center mx-4 w-auto">
+    <div className="p-4">
+      <p className="bg-white text-gray-800 text-lg font-medium p-4 rounded-md shadow-sm text-center mx-4 w-auto">
         {ris}
-    </p>
-      <table className="border border-gray-500 mt-4">
-        <thead>
-          <tr>
-            <th className="border border-gray-500"></th>
-            {giorniSettimana.map((giorno, index) => (
-              <th key={index} className="border border-gray-500">{giorno}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {oraInizio.map((ora, index) => (
-            <tr key={index}>
-              <td className="border border-gray-500 py-2 px-2">{`${ora} \n ${oraFine[index]}`}</td>
-              {giorniSettimana.map((giorno, dayIndex) => {
-                const aula = data[giorno][oraInizio[index]]?.AULA || '';
-                const doc = data[giorno][oraInizio[index]]?.DOC_COGN || '';
-                const mat = data[giorno][oraInizio[index]]?.MAT_NOME || '';
-                const classe = data[giorno][oraInizio[index]]?.CLASSE || '';
-                const coloreMateria = coloriMaterie[mat] || 'white';
-                
-                return (
-                  <td key={dayIndex} className="border border-gray-500 py-2 px-2">
-                    <div className="mb-1"><strong>{mat}</strong> {doc}</div>
-                    <div className="mb-1">{aula.replace(/[<>]/g, '')}</div>
-                    <div className="mb-1"><italic>{classe.replace(/[<>]/g, '').replace(/ALT-REL|ALTERNATIVA/g, ' ')}</italic></div>
-                  </td>
-                );
-              })}
+      </p>
+      <div className="overflow-x-auto mt-4">
+        <table className="min-w-full bg-white border-collapse">
+          <thead>
+            <tr>
+              <th className="border border-gray-500 px-4 py-2"></th>
+              {giorniSettimana.map((giorno, index) => (
+                <th key={index} className="border border-gray-500 px-4 py-2">{giorno}</th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {oraInizio.map((ora, index) => (
+              <tr key={index}>
+                <td className="border border-gray-500 px-4 py-2 whitespace-pre">{`${ora}\n${oraFine[index]}`}</td>
+                {giorniSettimana.map((giorno, dayIndex) => {
+                  const aula = data[giorno][oraInizio[index]]?.AULA || '';
+                  const doc = data[giorno][oraInizio[index]]?.DOC_COGN || '';
+                  const mat = data[giorno][oraInizio[index]]?.MAT_NOME || '';
+                  const classe = data[giorno][oraInizio[index]]?.CLASSE || '';
+                  const coloreMateria = coloriMaterie[mat] || 'white';
+
+                  return (
+                    <td key={dayIndex} className="border border-gray-500 px-4 py-2" style={{ backgroundColor: coloreMateria }}>
+                      <div className="mb-1 font-bold">{mat}</div>
+                      <div className="mb-1">{doc}</div>
+                      <div className="mb-1">{aula.replace(/[<>]/g, '')}</div>
+                      <div className="italic">{classe.replace(/[<>]/g, '').replace(/ALT-REL|ALTERNATIVA/g, ' ')}</div>
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
