79c00ec3034ead40bccdf95a9a411de8e494fa81
davide <davide.martinelli@jcmaxwell.it>
2024-06-06 09:51:27 +0200

    last update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,32 +1,26 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import AdminDashboard from "./pages/Admindashboard";
-import HomePage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Test from './pages/LoginPage';
 import Page404 from "./pages/Page404";
+import SchedulingTable from "./pages/Table";
+import Login from "./pages/LoginPage";
+import Admin from "./pages/Admindashboard";
+import '../src/index.css'; // Importa i fogli di stile globali
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/admin"
-          element={
-            <RequireAuth>
-              <AdminDashboard />
-            </RequireAuth>
-          }
-        />
-        <Route path="*" element={<Page404 />} />
+        <Route path="/" element={<Homepage />} /> {/* Homepage - DON'T TOUCH */}
+        <Route path="/test" element={<Test />} /> {/* Put any pages you want */}
+        <Route path="/table" element={<SchedulingTable />} /> {/* Schedule Table page */}
+        <Route path="/login" element={<Login />} /> {/* Schedule Table page */}
+        <Route path="/settings" element={<Admin />} /> {/* Schedule Table page */}
+        <Route path="*" element={<Page404 />} /> {/* Page 404 */}
       </Routes>
     </BrowserRouter>
   );
-};
-
-const RequireAuth = () => {
-  return <Navigate to="/login" replace />;
 };
 
 export default App;
